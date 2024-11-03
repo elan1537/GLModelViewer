99fc8abff0efe48207d0c92cb4ac7978c2381f8f
Sehyeon Park <elan1537@gmail.com>
2024-11-03 22:25:40 +0900

    [Chore] system.h, system.cpp 제거

--- system.h
+++
@@ -1,9 +0,0 @@
-#include "Angel.h"
-
-class system {
-    public:
-        static mat4 lookAt(vec3 eye, vec3 center, vec3 up);
-        static mat4 perspective(float fovy, float aspect, float zNear, float zFar);
-        static mat4 ortho(float left, float right, float bottom, float top, float zNear, float zFar);
-        static float toRadian(float degree);
-};
