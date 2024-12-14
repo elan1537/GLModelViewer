2529b95d1e24b9f1c6a19726bc803edcc29ef3ac
Sehyeon Park <elan1537@gmail.com>
2024-12-14 14:16:17 +0900

    [Feature] 카메라 패스 렌더링 추가

--- src/rendering/CubeRenderer.cpp
+++ src/rendering/CubeRenderer.cpp
@@ -3,69 +3,14 @@
 #include "CubeRenderer.h"
 #include <iostream>
 
-static float vertices[] = {
-    -0.5f, -0.5f, -0.5f,
-    0.5f, -0.5f, -0.5f,
-    0.5f, 0.5f, -0.5f,
-    0.5f, 0.5f, -0.5f,
-    -0.5f, 0.5f, -0.5f,
-    -0.5f, -0.5f, -0.5f,
-
-    -0.5f, -0.5f, 0.5f,
-    0.5f, -0.5f, 0.5f,
-    0.5f, 0.5f, 0.5f,
-    0.5f, 0.5f, 0.5f,
-    -0.5f, 0.5f, 0.5f,
-    -0.5f, -0.5f, 0.5f,
-
-    -0.5f, 0.5f, 0.5f,
-    -0.5f, 0.5f, -0.5f,
-    -0.5f, -0.5f, -0.5f,
-    -0.5f, -0.5f, -0.5f,
-    -0.5f, -0.5f, 0.5f,
-    -0.5f, 0.5f, 0.5f,
-
-    0.5f, 0.5f, 0.5f,
-    0.5f, 0.5f, -0.5f,
-    0.5f, -0.5f, -0.5f,
-    0.5f, -0.5f, -0.5f,
-    0.5f, -0.5f, 0.5f,
-    0.5f, 0.5f, 0.5f,
-
-    -0.5f, -0.5f, -0.5f,
-    0.5f, -0.5f, -0.5f,
-    0.5f, -0.5f, 0.5f,
-    0.5f, -0.5f, 0.5f,
-    -0.5f, -0.5f, 0.5f,
-    -0.5f, -0.5f, -0.5f,
-
-    -0.5f, 0.5f, -0.5f,
-    0.5f, 0.5f, -0.5f,
-    0.5f, 0.5f, 0.5f,
-    0.5f, 0.5f, 0.5f,
-    -0.5f, 0.5f, 0.5f,
-    -0.5f, 0.5f, -0.5f};
-
-static vec3 cubePositions[] = {
-    vec3(0.0f, 0.0f, -1.0f),
-    vec3(0.0f, 1.0f, 0.0f),
-    vec3(0.0f, 0.0f, 1.0f),
-    vec3(0.0f, -1.0f, 0.0f),
-    vec3(1.0f, 0.0f, 0.0f),
-    vec3(-1.0f, 0.0f, 0.0f)};
-
-std::vector<vec3> colors = {
-    vec3(1.0f, 0.0f, 0.0f),
-    vec3(0.0f, 1.0f, 0.0f),
-    vec3(0.0f, 0.0f, 1.0f),
-    vec3(1.0f, 1.0f, 0.0f),
-    vec3(0.0f, 1.0f, 1.0f),
-    vec3(1.0f, 0.0f, 1.0f)};
-
 CubeRenderer::CubeRenderer()
 {
+    // 생성 시 VAO/VBO 생성
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+
+    glGenVertexArrays(1, &sphereVAO);
+    glGenBuffers(1, &sphereVBO);
 
     shader = std::make_unique<Shader>("shaders/cube.vert", "shaders/cube.frag");
     std::cout << "Shader ID: " << shader->getID() << std::endl;
@@ -75,41 +20,130 @@ CubeRenderer::~CubeRenderer()
 {
     glDeleteBuffers(1, &VBO);
     glDeleteVertexArrays(1, &VAO);
+
+    glDeleteBuffers(1, &sphereVBO);
+    glDeleteVertexArrays(1, &sphereVAO);
 }
 
 bool CubeRenderer::Init(const std::vector<vec3> &positions)
 {
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // aPos at location 0 (vec3)
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-
     _positions = positions;
+    _lines.clear();
 
+    // positions 사이를 선분으로 이음
+    for (size_t i = 0; i + 1 < _positions.size(); ++i)
+    {
+        _lines.push_back(_positions[i]);
+        _lines.push_back(_positions[i + 1]);
+    }
+
+    std::cout << "lines size: " << _lines.size() << std::endl;
+    for (unsigned int i = 0; i < _lines.size(); i++)
+    {
+        std::cout << "line " << i << ": " << _lines[i] << std::endl;
+    }
+
+    // 라인용 VAO/VBO 설정
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * _lines.size(), _lines.data(), GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vec3), (void *)0);
     glBindVertexArray(0);
+
+    // 구 지오메트리 생성
+    // stacks=16, slices=16 정도로 설정 (적절히 변경 가능)
+    GenerateSphere(_sphereVertices, 16, 16, 1.0f);
+    _sphereVertCount = static_cast<GLsizei>(_sphereVertices.size());
+
+    // 구용 VAO/VBO 설정
+    glBindVertexArray(sphereVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, sphereVBO);
+    glBufferData(GL_ARRAY_BUFFER, _sphereVertices.size() * sizeof(vec3), _sphereVertices.data(), GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vec3), (void *)0);
+    glBindVertexArray(0);
+
     return true;
 }
 
 void CubeRenderer::Draw(const mat4 model, const mat4 view, const mat4 proj)
 {
-    shader->use(); // 이 쉐이더 프로그램으로 전환
+    shader->use();
 
     shader->setMat4("u_view", view);
     shader->setMat4("u_proj", proj);
 
+    // 먼저 라인 렌더링
+    shader->setVec3("u_color", vec3(1.0f, 0.0f, 0.0f)); // 라인 색상: 빨강
+    shader->setMat4("u_model", mat4(1.0f));
     glBindVertexArray(VAO);
+    glDrawArrays(GL_LINES, 0, static_cast<GLsizei>(_lines.size()));
 
-    for (unsigned int i = 0; i < _positions.size(); i++)
+    // 각 위치마다 구 렌더링
+    shader->setVec3("u_color", vec3(0.0f, 0.0f, 1.0f)); // 구 색상: 파랑
+    glBindVertexArray(sphereVAO);
+    for (auto &pos : _positions)
     {
-        mat4 m = Translate(_positions[i]);
-        m = m * Scale(vec3(0.05f, 0.05f, 0.05f));
+        // 위치 pos에 조그마한 구 렌더링
+        mat4 sphereModel = Translate(pos) * Scale(0.05f, 0.05f, 0.05f);
+        shader->setMat4("u_model", sphereModel);
+        glDrawArrays(GL_TRIANGLES, 0, _sphereVertCount);
+    }
 
-        shader->setMat4("u_model", m);
-        shader->setVec3("u_color", vec3(0.1, 0.1, 0.5));
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+    GLenum err = glGetError();
+    if (err != GL_NO_ERROR)
+    {
+        std::cout << "CubeRenderer::Draw OpenGL error: " << err << std::endl;
+    }
+}
+
+void CubeRenderer::GenerateSphere(std::vector<vec3> &sphereVerts, int stacks, int slices, float radius)
+{
+    // stacks: 위도 분할 횟수
+    // slices: 경도 분할 횟수
+    // 각도 계산
+    // 위도(theta): -π/2 ~ π/2
+    // 경도(phi): -π ~ π
+    // 삼각형 strip을 만들기 위해 각 세그먼트마다 두 개의 위도줄 사이를 연결
+
+    sphereVerts.clear();
+    for (int i = 0; i < stacks; i++)
+    {
+        float lat0 = M_PI * (-0.5f + (float)i / stacks);
+        float z0 = sin(lat0);
+        float zr0 = cos(lat0);
+
+        float lat1 = M_PI * (-0.5f + (float)(i + 1) / stacks);
+        float z1 = sin(lat1);
+        float zr1 = cos(lat1);
+
+        for (int j = 0; j < slices; j++)
+        {
+            float lng = 2.0f * M_PI * (float)(j) / slices;
+            float x = cos(lng);
+            float y = sin(lng);
+
+            float lng_next = 2.0f * M_PI * (float)(j + 1) / slices;
+            float x_next = cos(lng_next);
+            float y_next = sin(lng_next);
+
+            // 4점 정의
+            vec3 v0(x * zr0 * radius, y * zr0 * radius, z0 * radius);
+            vec3 v1(x * zr1 * radius, y * zr1 * radius, z1 * radius);
+            vec3 v2(x_next * zr1 * radius, y_next * zr1 * radius, z1 * radius);
+            vec3 v3(x_next * zr0 * radius, y_next * zr0 * radius, z0 * radius);
+
+            // 두 삼각형 (v0, v1, v2), (v0, v2, v3)
+            sphereVerts.push_back(v0);
+            sphereVerts.push_back(v1);
+            sphereVerts.push_back(v2);
+
+            sphereVerts.push_back(v0);
+            sphereVerts.push_back(v2);
+            sphereVerts.push_back(v3);
+        }
     }
 }
--- src/rendering/CubeRenderer.h
+++ src/rendering/CubeRenderer.h
@@ -14,7 +14,15 @@ public:
     void Draw(const mat4 model, const mat4 view, const mat4 proj);
 
 private:
+    void GenerateSphere(std::vector<vec3> &sphereVerts, int stacks, int slices, float radius);
+
     std::vector<vec3> _positions;
-    GLuint VAO, VBO;
+    std::vector<vec3> _lines;
+
+    GLuint VAO, VBO;             // For line rendering
+    GLuint sphereVAO, sphereVBO; // For sphere rendering
+
     std::unique_ptr<Shader> shader;
+    std::vector<vec3> _sphereVertices;
+    GLsizei _sphereVertCount = 0; // Number of vertices for the sphere
 };
